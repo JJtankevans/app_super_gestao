b0a2a3188ea0eaf7312f8edc9a67dc8bf133564d
JJtankevans <v.oliveira042997@gmail.com>
2021-12-20 16:33:14 -0400

    Fragmentando o código

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,23 +1,8 @@
 @extends('site.layouts.basico')
 
+@section('titulo', "Home")
+
 @section('content')
-    
-    <div class="topo">
-
-        <div class="logo">
-            <a href="{{ route('site.index') }}">
-                <img src={{ asset('img/logo.png') }}>
-            </a>
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
 
     <div class="conteudo-destaque">
 
