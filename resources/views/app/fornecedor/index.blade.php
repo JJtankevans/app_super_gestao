39b5aebacbc0197dc35e332a0492c413134c13e9
JJtankevans <v.oliveira042997@gmail.com>
2021-12-20 15:54:13 -0400

    Criação dos controllers e das views

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,7 @@
+<h3> Somente testando essa parada</h3>
+
+{{-- Fazendo comentário no blade--}}
+@foreach ( $fornecedores as $item)
+    Fornecedor: {{ $item }}
+    <hr>
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\FornecedorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,17 @@ use App\Http\Controllers\SobreNosController;
 */
 
 
-Route::get('/' ,[PrincipalController::class, 'principal']); 
+Route::get('/', [PrincipalController::class, 'principal'])->name("site.index"); 
+Route::get('/sobre_nos', [SobreNosController::class, 'sobreNos'])->name("site.sobrenos");
+Route::get('/contato', [ContatoController::class, 'contato'])->name("site.contato");
+Route::get('/login',function() { echo "Login (view)"; })->name("site.login");
 
-Route::get('/sobre_nos', [SobreNosController::class, 'sobreNos']);
+Route::prefix('/app') -> group(function() {
+    Route::get('/clientes', function() { echo "Client (view)";})->name("app.clientes");
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name("app.fornecedores");
+    Route::get('/produtos',function() { echo "Produtos (view)";})->name("app.produtos");
+});
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::fallback(function() {
+    echo "A rota acessada não existe, <a href= ".route('site.index').">clique aqui</a> para ir para a página inicial.";
+});
