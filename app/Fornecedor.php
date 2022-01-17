427343a0720b0d15c4cca8c66e8aeaebad1e2801
JJtankevans <v.oliveira042997@gmail.com>
2022-01-17 17:31:27 -0400

    SoftDeletes

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,13 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
     /*Fazer isso caso o nome da tabela no banco seja diferente do modelo presente aqui
     por exemplo fornecedors != fornecedores;
     protected $table = 'fornecedores';*/
-
+    use SoftDeletes;
+    
     //Permite a criação de um objeto usando um metodo estático 
     //no caso do curso foi o \App\Fornecedor::create()
     protected $fillable = ['nome', 'site', 'uf', 'email'];
