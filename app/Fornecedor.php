83bf3420bcb8dd9140a9eae5cc69a97722fe6094
JJtankevans <v.oliveira042997@gmail.com>
2022-01-17 03:13:15 -0400

    Usando o create e o fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -9,4 +9,8 @@ class Fornecedor extends Model
     /*Fazer isso caso o nome da tabela no banco seja diferente do modelo presente aqui
     por exemplo fornecedors != fornecedores;
     protected $table = 'fornecedores';*/
+
+    //Permite a criação de um objeto usando um metodo estático 
+    //no caso do curso foi o \App\Fornecedor::create()
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
