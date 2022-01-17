af9de50990df3f79994422c6622f3e985e2c8b1b
JJtankevans <v.oliveira042997@gmail.com>
2022-01-17 03:03:21 -0400

    Começando a usar o eloquent ORM

---
+++ app/Fornecedor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fornecedor extends Model
+{
+    /*Fazer isso caso o nome da tabela no banco seja diferente do modelo presente aqui
+    por exemplo fornecedors != fornecedores;
+    protected $table = 'fornecedores';*/
+}
