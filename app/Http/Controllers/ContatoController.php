ffb2e46d8345c86bd7112caf9a05401fac25adfc
JJtankevans <v.oliveira042997@gmail.com>
2022-01-19 11:07:14 -0400

    Pegando informacoes do formulario

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $req) {
 
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $req->input('nome');
+        $contato->telefone = $req->input('telefone');
+        $contato->email = $req->input('email');
+        $contato->motivo_contato = $req->input('motivo_contato');
+        $contato->mensagem = $req->input('mensagem');
+
+        $contato->save();
+
+         OUTRA FORMA DE SALVAR OS ATRIBUTOS NO BANCO É A SEGUINTE:
+         ____________________________________
+        |                                    |
+        |  $contato = new SiteContato();     |
+        |  $contato->fill($request->all());  |
+        |  Ou podemos usar o método          |
+        |  $contato->create($request->all());|
+        |  $contato->save();                 |
+        |____________________________________|
+        
+        Porém para fazer dessas duas maneiras é necessário habilitar a váriavel $fillable
+        dentro do model que deve ser salvo;
+        */
         return view('site.contato');
+    }
+
+    public function store(Request $req) {
+        SiteContato::create($req->all());
     }
 }
