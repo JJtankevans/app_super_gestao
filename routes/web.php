227c9f917c07924553acafb32a6c6fe6b8e5e282
JJtankevans <v.oliveira042997@gmail.com>
2022-01-25 17:43:04 -0400

    Primeira aula de middleware

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ use App\Http\Controllers\FornecedorController;
 */
 
 
-Route::get('/', [PrincipalController::class, 'principal'])->name("site.index"); 
+Route::get('/', [PrincipalController::class, 'principal'])->name("site.index")->middleware(LogAcessoMiddleware::class); 
 Route::get('/sobre_nos', [SobreNosController::class, 'sobreNos'])->name("site.sobrenos");
 Route::get('/contato', [ContatoController::class, 'contato'])->name("site.contato");
 Route::post('/contato', [ContatoController::class, 'store'])->name("site.contato");
